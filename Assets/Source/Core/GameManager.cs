6630da5c2e5179a1e89bf711bfcb2da2b41f966b
pkokanov <pkokanov@gmail.com>
2017-11-19 16:39:27 +0200

    First big commit

---
+++ Assets/Source/Core/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public sealed class GameManager {
+
+    public static GameManager instance = new GameManager();
+
+    private bool gameStarted;
+
+    private GameManager() {
+        gameStarted = false;
+    }
+
+    public static GameManager Instance {
+        get {
+            return instance;
+        }
+    }
+
+
+    public bool GameStarted {
+        get {
+            return gameStarted;
+        }
+        set {
+            this.gameStarted = value;
+        }
+    }
+}
---
+++ Assets/Source/Core/MainMenuUIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIController : MonoBehaviour {
+
+    public GameObject resumeButton;
+
+    void Awake() {
+    }
+
+    private void Start() {
+        if (GameManager.Instance.GameStarted && resumeButton) {
+            resumeButton.SetActive(true);
+        } else {
+            resumeButton.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
